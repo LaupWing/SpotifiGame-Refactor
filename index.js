4e258d389854556f688d90e68ecbf1da5690f46b
LaupWing <loc_strict@hotmail.com>
2023-04-19 09:21:31 +0200

    udpate

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const port = process.env.PORT || 3000
+
+app
+   .set("view engine", "ejs")
+   .set("views", "view")
